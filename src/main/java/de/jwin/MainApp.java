dfc30b7df01cb68fc348d11a07a5ef87fd09aca4
jwin <jwin1968@gmail.com>
2016-04-25 22:30:39 +0200

    when does a threadpool die ?

--- src/main/java/de/jwin/MainApp.java
+++ src/main/java/de/jwin/MainApp.java
@@ -1,13 +1,22 @@
 package de.jwin;
 
-import org.apache.camel.main.Main;
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
 
 public class MainApp {
 
-    public static void main(String... args) throws Exception {
-        Main main = new Main();
+    public static void main(final String... args) throws Exception {
+
+        CamelContext context = new DefaultCamelContext();
+        context.addRoutes(new RouteBuilder());
+        context.start();
+        Thread.sleep(10000);
+        context.stop();
+/*
+        final Main main = new Main();
         main.addRouteBuilder(new RouteBuilder());
         main.run(args);
+*/
     }
 
 }
