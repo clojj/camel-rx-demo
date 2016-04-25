95cb192b31f94a73d59d3ea020fd8a2e34d818f2
jwin <jwin1968@gmail.com>
2016-04-25 22:36:10 +0200

    fix FindBugs

--- src/main/java/de/jwin/MainApp.java
+++ src/main/java/de/jwin/MainApp.java
@@ -8,13 +8,13 @@ public class MainApp {
     public static void main(final String... args) throws Exception {
 
         CamelContext context = new DefaultCamelContext();
-        context.addRoutes(new RouteBuilder());
+        context.addRoutes(new RxDemoRouteBuilder());
         context.start();
         Thread.sleep(10000);
         context.stop();
 /*
         final Main main = new Main();
-        main.addRouteBuilder(new RouteBuilder());
+        main.addRouteBuilder(new RxDemoRouteBuilder());
         main.run(args);
 */
     }
