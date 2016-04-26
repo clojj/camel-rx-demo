8f6c3c6c0a85ebf274c1cd267457fa30d2b7892d
jwin <jwin1968@gmail.com>
2016-04-26 22:47:16 +0200

    metrics added

--- src/main/java/de/jwin/MainApp.java
+++ src/main/java/de/jwin/MainApp.java
@@ -1,22 +1,21 @@
 package de.jwin;
 
-import org.apache.camel.CamelContext;
-import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.main.Main;
 
 public class MainApp {
 
     public static void main(final String... args) throws Exception {
 
+/*
         CamelContext context = new DefaultCamelContext();
         context.addRoutes(new RxDemoRouteBuilder());
         context.start();
         Thread.sleep(10000);
         context.stop();
-/*
+*/
         final Main main = new Main();
         main.addRouteBuilder(new RxDemoRouteBuilder());
         main.run(args);
-*/
     }
 
 }
--- src/main/java/de/jwin/RxDemoRouteBuilder.java
+++ src/main/java/de/jwin/RxDemoRouteBuilder.java
@@ -1,24 +1,21 @@
 package de.jwin;
 
-import com.google.common.util.concurrent.MoreExecutors;
 import org.apache.camel.Message;
+import org.apache.camel.component.metrics.routepolicy.MetricsRoutePolicyFactory;
 import org.apache.camel.rx.ReactiveCamel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import rx.Observable;
-import rx.observables.ConnectableObservable;
 import rx.schedulers.Schedulers;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 public class RxDemoRouteBuilder extends org.apache.camel.builder.RouteBuilder {
 
     private static final Logger LOG = LoggerFactory.getLogger(RxDemoRouteBuilder.class);
 
-    //    private Executor executor = Executors.newFixedThreadPool(3);
+    private Executor executor = Executors.newFixedThreadPool(3);
 /*
     private Executor executor = Executors.newFixedThreadPool(3,
         r -> {
@@ -27,40 +24,53 @@ public class RxDemoRouteBuilder extends org.apache.camel.builder.RouteBuilder {
             return t;
         });
 */
+/*
     private Executor executor = MoreExecutors.getExitingExecutorService(
-            (ThreadPoolExecutor) Executors.newFixedThreadPool(4),
-            3, TimeUnit.SECONDS //period after which executor will be automatically closed
-    );
+			(ThreadPoolExecutor) Executors.newFixedThreadPool(4),
+			3, TimeUnit.SECONDS //period after which executor will be automatically closed
+	);
+*/
 
     private int counter;
 
     public void configure() throws Exception {
-
         from("timer:fast-timer?period=1")
+                .to("metrics:timer:simple.timer?action=start")
                 .process(exchange -> {
+                    Thread.sleep(500);
                     exchange.getOut().setBody(counter++);
                 })
-                .to("direct:rx-in");
+                .to("direct:rx-in")
+                .to("metrics:timer:simple.timer?action=stop");
+
+
+        final MetricsRoutePolicyFactory routePolicyFactory = new MetricsRoutePolicyFactory();
+        routePolicyFactory.setUseJmx(true);
+        getContext().addRoutePolicyFactory(routePolicyFactory);
 
         getContext().addStartupListener((camelContext, b) -> {
 
             ReactiveCamel rx = new ReactiveCamel(camelContext);
             final Observable<Message> messageObservable = rx.toObservable("direct:rx-in");
+/*
             ConnectableObservable<Message> connectableObservable = messageObservable.publish();
             connectableObservable.connect();
+			connectableObservable
+*/
 
-            connectableObservable
+            messageObservable
+//					.onBackpressureDrop()
                     .onBackpressureDrop(message -> LOG.warn("DROPPED {}", message.getBody()))
-//                    .onBackpressureDrop()
-                    .observeOn(Schedulers.from(executor)).forEach(message -> {
-                try {
-                    Thread.sleep(20);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                    .observeOn(Schedulers.from(executor))
+                    .forEach(message -> {
+                        try {
+                            Thread.sleep(10);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
 
-                LOG.info("counter {} thread {}", message.getBody(), Thread.currentThread().getName());
-            });
+                        LOG.info("counter {} thread {}", message.getBody(), Thread.currentThread().getName());
+                    });
         });
     }
 
